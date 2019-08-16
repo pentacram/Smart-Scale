a146471d4c21e79f8f7dbaf98560b7727aa936da
penta <tofiq.cabbarov93@gmail.com>
2019-08-16 16:14:43 +0400

    serial

--- scale_set/serials.py
+++ scale_set/serials.py
@@ -1,3 +1,4 @@
+from threading import Thread
 import serial
 from django.utils import timezone
 from .models import *
@@ -5,21 +6,28 @@ ser = serial.Serial('COM22',19200,timeout=1)
 while True:
     new_data = ser.read(100)
     if new_data:
-        new_data = new_data.decode('utf-8')       
+        new_data = new_data.decode('utf-8')
         new_data= new_data.split(',')
         current =InfoFields.objects.filter(number=new_data[0],weight=float(new_data[1][1:])).last()
         if current:
-            InfoFields.objects.create(
-                number = new_data[0],
-                weight=float(new_data[1][1:]),
-                age=current.age,
-                gender=current.gender,
-                feed =current.feed,
-                born_date = current.born_date
-            )
+            def updating():
+                InfoFields.objects.create(
+                    number=new_data[0],
+                    weight=float(new_data[1][1:]),
+                    age=current.age,
+                    gender=current.gender,
+                    feed=current.feed,
+                    born_date=current.born_date
+                )
+            background_job = Thread(target=updating())
+            background_job.start()
+            print('ok')
         else:
-            InfoFields.objects.create(
-                number=new_data[0],
-                weight=float(new_data[1][1:])
-            )
-        
+            def creating():
+                InfoFields.objects.create(
+                    number=new_data[0],
+                    weight=float(new_data[1][1:])
+                )
+            background_job = Thread(target=creating())
+            background_job.start()
+            print('ok')
